c56d55979ea741f1db69a0c9a501a06258bf4ff2
Jen Luker <1584489+knitcodemonkey@users.noreply.github.com>
2018-12-22 14:28:15 -0700

    Update find-me.js

--- find-me.js
+++ find-me.js
@@ -1,101 +1,102 @@
 const speakingEngagements = [
   {
-		date: 'Oct 24-25, 2018',
-		confUrl: 'https://conf.reactjs.org/',
-		confName: 'React Conf',
-    confLocation: 'Henderson, NV',
+    date: "Oct 24-25, 2018",
+    confUrl: "https://conf.reactjs.org/",
+    confName: "React Conf",
+    confLocation: "Henderson, NV",
     talkType: "Organizer",
-		talkName: "Fiber Arts Circle",
-	},
-	{
-		date: 'Sep 29-30, 2018',
-		confUrl: 'http://www.reactboston.com/',
-		confName: 'React Boston',
-    confLocation: 'Boston, MA',
-    talkType: "Session",
-		talkName: "How 'Forbidden Planet' Cured My Imposter Syndrome",
-    talkVideo: "https://youtu.be/Yk53eWIu7Nw"
-	},
-	{
-		date: 'Sep 21, 2018',
-		confUrl: 'https://conf.utahjs.com/',
-		confName: 'UtahJS Conf',
-    confLocation: 'Sandy, UT',
-    talkType: "Panel",
-		talkName: 'Discussion Panel: Accessible Technology - Tommy Edison, Jen Luker, Joel Saupe, Kent C. Dodds',
-		talkVideo: 'https://youtu.be/4jPwU-5gAUc',
-	},
-	{
-		date: 'Sep 21, 2018',
-		confUrl: 'https://conf.utahjs.com/',
-		confName: 'UtahJS Conf',
-    confLocation: 'Sandy, UT',
-    talkType: "Session",
-		talkName: 'Accessibility: The Sprint-Filler Way',
-		talkVideo: 'https://youtu.be/5fJxJ6uGD0c',
-	},
-	{
-		date: 'Sep 7-8, 2018',
-		confUrl: 'https://www.reactathon.com/',
-		confName: 'Reactathon',
-    confLocation: 'San Francisco, CA',
-    talkType: "Keynote",
-		talkName: 'React Accessibility: Beyond the Basics',
-		talkVideo: 'https://youtu.be/8E9AEZjglqI',
-	},
-	{
-		date: 'Jul 12-13, 2018',
-		confUrl: 'https://infinite.red/ChainReactConf',
-		confName: 'Chain React',
-    confLocation: 'Portland, OR',
-		talkType: 'Panel',
-    talkName: "Chain React 2018: Panel",
-		talkVideo: 'https://youtu.be/iV-1gYCNhkQ',
-	},
-	{
-		date: 'Jul 12-13, 2018',
-		confUrl: 'https://infinite.red/ChainReactConf',
-		confName: 'Chain React',
-    confLocation: 'Portland, OR',
-    talkType: "Session",
-		talkName: 'Be a React Native A11y',
-		talkVideo: 'https://youtu.be/cfzFZIouydY',
-	},
-	{
-		date: 'Jun 29, 2018',
-		confUrl: 'http://anywherejs.com',
-		confName: 'AnywhereJs',
-    confLocation: 'Online',
-    talkType: "Session",
-		talkName: 'Accessibility: The Sprint-Filler Way',
-		talkVideo: 'https://youtu.be/Uf4HaOZCfVQ',
-	},
-	{
-		date: 'Jun 6-8, 2018',
-		confUrl: 'https://2018.syntaxcon.com/',
-		confName: 'SyntaxCon',
-    confLocation: 'Charleston, SC',
-    talkType: "Session",
-		talkName: 'Styling React for Reuse',
-	},
+    talkName: "Fiber Arts Circle"
+  },
   {
-		date: 'May 17, 2018',
-		confUrl: 'http://deseretdigital.com/',
-		confName: 'DDM GAAD Event',
-    confLocation: 'Salt Lake City, UT',
+    date: "Sep 29-30, 2018",
+    confUrl: "http://www.reactboston.com/",
+    confName: "React Boston",
+    confLocation: "Boston, MA",
     talkType: "Session",
-		talkName: 'Styling React for Reuse',
+    talkName: "How 'Forbidden Planet' Cured My Imposter Syndrome",
+    talkVideo: "https://youtu.be/Yk53eWIu7Nw"
+  },
+  {
+    date: "Sep 21, 2018",
+    confUrl: "https://conf.utahjs.com/",
+    confName: "UtahJS Conf",
+    confLocation: "Sandy, UT",
+    talkType: "Panel",
+    talkName:
+      "Discussion Panel: Accessible Technology - Tommy Edison, Jen Luker, Joel Saupe, Kent C. Dodds",
+    talkVideo: "https://youtu.be/4jPwU-5gAUc"
+  },
+  {
+    date: "Sep 21, 2018",
+    confUrl: "https://conf.utahjs.com/",
+    confName: "UtahJS Conf",
+    confLocation: "Sandy, UT",
+    talkType: "Session",
+    talkName: "Accessibility: The Sprint-Filler Way",
+    talkVideo: "https://youtu.be/5fJxJ6uGD0c"
+  },
+  {
+    date: "Sep 7-8, 2018",
+    confUrl: "https://www.reactathon.com/",
+    confName: "Reactathon",
+    confLocation: "San Francisco, CA",
+    talkType: "Keynote",
+    talkName: "React Accessibility: Beyond the Basics",
+    talkVideo: "https://youtu.be/8E9AEZjglqI"
+  },
+  {
+    date: "Jul 12-13, 2018",
+    confUrl: "https://infinite.red/ChainReactConf",
+    confName: "Chain React",
+    confLocation: "Portland, OR",
+    talkType: "Panel",
+    talkName: "Chain React 2018: Panel",
+    talkVideo: "https://youtu.be/iV-1gYCNhkQ"
+  },
+  {
+    date: "Jul 12-13, 2018",
+    confUrl: "https://infinite.red/ChainReactConf",
+    confName: "Chain React",
+    confLocation: "Portland, OR",
+    talkType: "Session",
+    talkName: "Be a React Native A11y",
+    talkVideo: "https://youtu.be/cfzFZIouydY"
+  },
+  {
+    date: "Jun 29, 2018",
+    confUrl: "http://anywherejs.com",
+    confName: "AnywhereJs",
+    confLocation: "Online",
+    talkType: "Session",
+    talkName: "Accessibility: The Sprint-Filler Way",
+    talkVideo: "https://youtu.be/Uf4HaOZCfVQ"
+  },
+  {
+    date: "Jun 6-8, 2018",
+    confUrl: "https://2018.syntaxcon.com/",
+    confName: "SyntaxCon",
+    confLocation: "Charleston, SC",
+    talkType: "Session",
+    talkName: "Styling React for Reuse"
+  },
+  {
+    date: "May 17, 2018",
+    confUrl: "http://deseretdigital.com/",
+    confName: "DDM GAAD Event",
+    confLocation: "Salt Lake City, UT",
+    talkType: "Session",
+    talkName: "Styling React for Reuse",
     talkVideo: "https://www.ksl.com/?sid=46322673"
-	},
-	{
-		date: 'Sep 18, 2017',
-		confUrl: 'https://conf.utahjs.com/',
-		confName: 'UtahJS Conf',
-    confLocation: 'Sandy, UT',
-    talkType: 'Session',
-		talkName: 'Styling React for Reuse',
-		talkVideo: 'https://youtu.be/1H8TO1fVuLY',
-	},
+  },
+  {
+    date: "Sep 18, 2017",
+    confUrl: "https://conf.utahjs.com/",
+    confName: "UtahJS Conf",
+    confLocation: "Sandy, UT",
+    talkType: "Session",
+    talkName: "Styling React for Reuse",
+    talkVideo: "https://youtu.be/1H8TO1fVuLY"
+  }
 ];
 
 export default speakingEngagements;
