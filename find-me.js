c4a4d035410d7c7ef9ff7c9b14ca28ae4e6c902d
Jen Luker <1584489+knitcodemonkey@users.noreply.github.com>
2018-12-22 12:11:10 -0700

    Update and rename find-me.json to find-me.js

---
+++ find-me.js
@@ -0,0 +1,66 @@
+const speakingEngagements = [
+	{
+		date: 'Sep 29-30, 2018',
+		confUrl: 'http://www.reactboston.com/',
+		confName: 'React Boston',
+		talkName: "How 'Forbidden Planet' Cured My Imposter Syndrome",
+    talkVideo: "https://youtu.be/Yk53eWIu7Nw"
+	},
+	{
+		date: 'Sep 21, 2018',
+		confUrl: 'https://conf.utahjs.com/',
+		confName: 'UtahJS Conf',
+		talkName: 'Discussion Panel: Accessible Technology - Tommy Edison, Jen Luker, Joel Saupe, Kent C. Dodds',
+		talkVideo: 'https://youtu.be/4jPwU-5gAUc',
+	},
+	{
+		date: 'Sep 21, 2018',
+		confUrl: 'https://conf.utahjs.com/',
+		confName: 'UtahJS Conf',
+		talkName: 'Accessibility: The Sprint-Filler Way',
+		talkVideo: 'https://youtu.be/5fJxJ6uGD0c',
+	},
+	{
+		date: 'Sep 7-8, 2018',
+		confUrl: 'https://www.reactathon.com/',
+		confName: 'Reactathon',
+		talkName: 'React Accessibility: Beyond the Basics',
+		talkVideo: 'https://youtu.be/8E9AEZjglqI',
+	},
+	{
+		date: 'Jul 12-13, 2018',
+		confUrl: 'https://infinite.red/ChainReactConf',
+		confName: 'Chain React',
+		talkName: 'Panel',
+		talkVideo: 'https://youtu.be/iV-1gYCNhkQ',
+	},
+	{
+		date: 'Jul 12-13, 2018',
+		confUrl: 'https://infinite.red/ChainReactConf',
+		confName: 'Chain React',
+		talkName: 'Be a React Native A11y',
+		talkVideo: 'https://youtu.be/cfzFZIouydY',
+	},
+	{
+		date: 'Jun 29, 2018',
+		confUrl: 'http://anywherejs.com',
+		confName: 'AnywhereJs',
+		talkName: 'Accessibility: The Sprint-Filler Way',
+		talkVideo: 'https://youtu.be/Uf4HaOZCfVQ',
+	},
+	{
+		date: 'Jun 6-8, 2018',
+		confUrl: 'https://2018.syntaxcon.com/',
+		confName: 'SyntaxCon',
+		talkName: 'Styling React for Reuse',
+	},
+	{
+		date: 'Sep 18, 2017',
+		confUrl: 'https://conf.utahjs.com/',
+		confName: 'UtahJS Conf',
+		talkName: 'Styling React for Reuse',
+		talkVideo: 'https://youtu.be/1H8TO1fVuLY',
+	},
+];
+
+export default speakingEngagements;
